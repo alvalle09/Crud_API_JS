fbcde6311099443b6d86c4a6b24ddf8a1b82ea0d
alvalle09 <phredvalle@gmail.com>
2020-12-10 23:45:16 -0600

    PATCH a record

--- routes/products.js
+++ routes/products.js
@@ -11,6 +11,10 @@ module.exports = function (db) {
     res.send(db.get("products").insert(newProduct).write());
   });
 
+  router.patch("/products/:id", (req, res) => {
+    res.send(db.get("products").find({id: req.params.id}).assign(req.body).write());
+  });
+
   return router;
 };
 

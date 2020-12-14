2f1ae14a540ae9b73ec4b0af146f6bc40b4e1356
alvalle09 <phredvalle@gmail.com>
2020-12-13 22:10:28 -0600

    switch to axios for api call'

--- public/javascripts/read.js
+++ public/javascripts/read.js
@@ -1,12 +1,6 @@
 
 document.getElementById("load").onclick = function() {
-  const req = new XMLHttpRequest();
-  req.open('GET', '/api/products');
-  req.onload = function() {
-    const data =JSON.parse(req.response);
-    addList({ data });
-  }
-  req.send();
+  axios.get('/api/products').then(addList);
 }
 
 function addList({ data }) {

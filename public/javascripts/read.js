1b54dfec691d280580dad5b719e95495f6cb5a7a
alvalle09 <phredvalle@gmail.com>
2020-12-13 21:45:17 -0600

    implement frontend js api calls

--- public/javascripts/read.js
+++ public/javascripts/read.js
@@ -1,4 +1,14 @@
 
+document.getElementById("load").onclick = function() {
+  const req = new XMLHttpRequest();
+  req.open('GET', '/api/products');
+  req.onload = function() {
+    const data =JSON.parse(req.response);
+    addList({ data });
+  }
+  req.send();
+}
+
 function addList({ data }) {
   resetContentArea();
 

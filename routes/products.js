a76d4e66b975f6d92b4b1d50e7b6d12964c55de3
alvalle09 <phredvalle@gmail.com>
2020-12-13 21:20:06 -0600

    user router.route to clean up code'

--- routes/products.js
+++ routes/products.js
@@ -2,31 +2,30 @@ var express = require("express");
 var router = express.Router();
 
 module.exports = function (db) {
-  router.get("/products", (req, res) => {
+  router.route("/products")
+    .get((req, res) => {
     res.send(db.get("products").value());
-  });
-
-  router.post("/products", (req, res) => {
+  })
+  .post((req, res) => {
     const newProduct = req.body;
     res.send(db.get("products").insert(newProduct).write());
-  });
+  })
 
   router.patch("/products/:id", (req, res) => {
     res.send(db.get("products").find({id: req.params.id}).assign(req.body).write());
   });
 
-  router.delete("/products/:id", (req, res) => {
+  router.route('/products/:id')
+  .delete((req, res) => {
     db.get("products").remove({id: req.params.id}).write();
     res.status(204).send();
-  });
-
-  router.get("/products/:id", (req, res) => {
+  })
+  .get((req, res) => {
     res.send(
       db.get("products").find({id: req.params.id}).value()
     );
   });
-
-
+  
   return router;
 };
 
